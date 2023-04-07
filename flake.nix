9a22f1d01e5150dbc30f792e2cdd669b41b2bd07
Ingo Ruhnke <grumbel@gmail.com>
2023-04-07 12:34:55 +0200

    Remove stable_diffusion_webui_src input

--- flake.nix
+++ flake.nix
@@ -4,14 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
-
-    stable_diffusion_webui_src = {
-      url = "github:AUTOMATIC1111/stable-diffusion-webui";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, stable_diffusion_webui_src, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
